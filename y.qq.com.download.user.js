3cc9fadb177216afb0dd8bd06a87e4bc30088ca7
JianjunMao <justmao945@gmail.com>
2012-07-23 21:04:42 +0800

    Add fuck fm.qq.com download button. -____________-b

--- y.qq.com.download.user.js
+++ y.qq.com.download.user.js
@@ -2,25 +2,38 @@
 // @name      QQ Music Download Button
 // @author    justmao945 AT gmail DOT com
 // @match     http://y.qq.com/
+// @match     http://fm.qq.com/
 // @run_at    document_end
 // ==/UserScript==
+
+is_fm = function(){
+  return location.href.indexOf("fm") !== -1;
+}
 
 add_btn_down = function(){
   if( document.getElementsByClassName("btn_down").length == 0 )
     document.getElementsByClassName("music_op")[0].innerHTML += "<strong class=btn_down title=Download onclick=\"this.getElementsByTagName('a')[0].click();\"> <a href=# onclick=\"this.setAttribute('href', document.getElementById('h5audio_media').src); this.setAttribute('download', g_topPlayer.getCurSongInfo().msinger + ' - ' + g_topPlayer.getCurSongInfo().msong);\"></a> </strong>";
-
 };
 
-ck_complete = setInterval( function(){
+
+ck_complete = setInterval( is_fm()
+? function(){
+   if( document.readyState === "complete" ){
+    clearInterval(ck_complete);
+    document.getElementsByClassName("menu")[0].getElementsByTagName("ul")[0].innerHTML += "<li><a id=btn_down href=# onclick=\"this.setAttribute('href', document.getElementById('h5audio_media').src); this.setAttribute('download', document.getElementById('divsongname').getAttribute('title'));\" onMouseOver=\"this.setAttribute('title', document.getElementById('divsongname').getAttribute('title'));\">Download</a></li>";
+  }
+}
+: function(){
   if( document.readyState === "complete" ){
     clearInterval(ck_complete);
     document.getElementsByClassName('play_bt')[0].addEventListener('click', add_btn_down);
     document.getElementsByClassName('prev_bt')[0].addEventListener('click', add_btn_down);
     document.getElementsByClassName('next_bt')[0].addEventListener('click', add_btn_down);
   }
-} ,500);
+} ,100);
 
-ck_audio = setInterval( function(){
+
+ck_audio = is_fm() ? null : setInterval( function(){
   h5audio = document.getElementById('h5audio_media');
   if( h5audio ){
     clearInterval(ck_audio);

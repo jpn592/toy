35942b0b75675b728cc586c91d54eddc0027107a
JianjunMao <justmao945@gmail.com>
2013-01-16 17:07:59 +0800

    update

--- fm.user.js
+++ fm.user.js
@@ -5,50 +5,47 @@
 // @run_at document_end
 // ==/UserScript==
 //
-// Press Ctrl + Down Arrow or call function qqdl() to download.
+// Press Ctrl + Down Arrow to download.
 //
 // [1] http://stackoverflow.com/questions/10485992/hijacking-a-variable-with-a-userscript-for-chrome
 // [2] http://www.catswhocode.com/blog/using-keyboard-shortcuts-in-javascript
 
-var CTRL = 17;
-var DOWN = 40;
+var code = function(){
+  var CTRL = 17;
+  var DOWN = 40;
 
-var dl = function(){
-  var link  = document.createElement('a');
-  var media = document.getElementById('h5audio_media');
-  var song  = document.getElementById('divsongname');
-  if( media && song ){
-    link.setAttribute('href', media.src);
-    link.setAttribute('download', song.getAttribute('title') + '.mp3');
+  var dl = function(){
+    var song = g_webPlayer.getSongInfoObj();
+    var link  = document.createElement('a');
+    link.setAttribute('href', g_webPlayer.getSongUrl(song));
+    link.setAttribute('download', song.msinger + ' - ' + song.msong + '.mp3');
     link.click();
   }
-}
 
-dl.isCtrl = false;
+  dl.isCtrl = false;
 
-var onkeyup = document.onkeyup;
-document.onkeyup = function(e) {
-  if(e.which === CTRL)
-    dl.isCtrl=false;
-
-  if( onkeyup && typeof(onkeyup) === 'function' )
-    onkeyup(e);
-}
-
-var onkeydown = document.onkeydown;
-document.onkeydown = function(e){
-  if(e.which === CTRL){
-    dl.isCtrl = true;
-  }
-  else if(e.which === DOWN && dl.isCtrl) {
-    dl();
+  var onkeyup = document.onkeyup;
+  document.onkeyup = function(e) {
+    if(e.which === CTRL)
+      dl.isCtrl=false;
+    if( onkeyup && typeof(onkeyup) === 'function' )
+      onkeyup(e);
   }
 
-  if( onkeydown && typeof(onkeydown) === 'function' )
-    onkeydown(e);
+  var onkeydown = document.onkeydown;
+  document.onkeydown = function(e){
+    if(e.which === CTRL){
+      dl.isCtrl = true;
+    }
+    else if(e.which === DOWN && dl.isCtrl) {
+      dl();
+    }
+    if( onkeydown && typeof(onkeydown) === 'function' )
+      onkeydown(e);
+  }
 }
 
 var script = document.createElement('script');
-script.textContent = 'var qqdl = ' + dl + ';';
+script.textContent = '(' + code + ')()';
 (document.head||document.documentElement).appendChild(script);
 script.parentNode.removeChild(script);
